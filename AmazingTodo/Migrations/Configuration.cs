d57b54663cccb3ec03d0894d91af279dedc4c3ee
pickotakus <pr.mish@gmail.com>
2015-09-28 01:04:15 +0300

    Ble

--- AmazingTodo/Migrations/Configuration.cs
+++ AmazingTodo/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace AmazingTodo.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(AmazingTodo.Models.AmazingTodoContext context)
